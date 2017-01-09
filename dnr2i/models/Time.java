cd655c9c761fe7b50f04c48544b463510f8267a8
hivinau.graffe <h.graffe@futurdigital.fr>
2017-01-09 15:38:54 +0100

    refact build.xml properties

---
+++ dnr2i/models/Time.java
@@ -0,0 +1,27 @@
+package dnr2i.models;
+
+import java.util.*;
+import dnr2i.models.entity.*;
+
+public final class Time {
+
+	private final List<TimeEntity> entities;
+	
+	public Time() {
+		
+		entities = new ArrayList<>();
+	}
+	
+	public boolean add(TimeEntity entity) {
+		
+		boolean entityAdded = false;
+		if(!entities.contains(entity)) {
+			
+			entities.add(entity);
+			entityAdded = true;
+		}
+		
+		return entityAdded;
+	}
+}
+

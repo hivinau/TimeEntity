2456358fd9a8d31987672b3a7c0344bf5006122f
alimux <alexducreux14@gmail.com>
2017-01-09 11:36:33 +0100

    modifying package name

--- dnr2i/models/TimeEntity.java
+++ dnr2i/models/TimeEntity.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package src.dnr2i.models;
+package dnr2i.models;
 
 /**
  *
  * @author user
  */
-public class GPSEntity {
+public abstract class TimeEntity {
+    
+    
     
 }
